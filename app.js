1e7706fdaa17f123cf4c18c97f5e4d709e0abd29
Theo Jolly <theonjolly@gmail.com>
2022-11-16 15:37:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ import cors from 'cors'
 import HelloController from './controllers/hello-controller.js';
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-
 const app = express()
 app.use(cors())
 app.use(express.json()) // parse JSON from request body
