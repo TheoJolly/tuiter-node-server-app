feb1a641d2b385553995067ebeaa5d6631adc763
Theo Jolly <theonjolly@gmail.com>
2022-11-27 15:54:20 -0500

    jkgytfd

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ import cors from 'cors'
 import HelloController from './controllers/hello-controller.js';
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/tuiter");
 const app = express()
 app.use(cors())
 app.use(express.json()) // parse JSON from request body
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,5 +1,4 @@
-import posts from "./tuits.js";
-let tuits = posts;
+import * as tuitsDao from '../../tuits/tuits-dao.js'
 
 const TuitController = (app) => {
 	app.get('/api/tuits', findTuits);
@@ -8,31 +7,30 @@ const TuitController = (app) => {
 	app.put('/api/tuits/:tid', updateTuit);
 }
 
-const findTuits  = (req, res) => {
+const findTuits  = async (req, res) => {
+	const tuits = await tuitsDao.findTuits();
 	res.json(tuits);
 }
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
 	const newTuit = req.body;
-	newTuit._id = Number((new Date()).getTime() + '');
 	newTuit.likes = 0;
 	newTuit.liked = false;
-	tuits.push(newTuit);
-	res.json(newTuit);
+	const insertedTuit = await tuitsDao.createTuit(newTuit);
+	res.json(insertedTuit);
 }
 
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
 	const tuitdIdToDelete = req.params['tid'];
-	tuits = tuits.filter((t) => String(t._id) !== tuitdIdToDelete);
+	const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
 	res.sendStatus(200);
 }
 
-const updateTuit = (req, res) => {
+const updateTuit = async (req, res) => {
 	const tuitdIdToUpdate = req.params.tid;
 	const updates = req.body;
-	const tuitIndex = tuits.findIndex((t) => String(t._id) === tuitdIdToUpdate)
-	tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
-	res.sendStatus(200);
+	const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
+	res.json(status);
 }
 
 export default TuitController
